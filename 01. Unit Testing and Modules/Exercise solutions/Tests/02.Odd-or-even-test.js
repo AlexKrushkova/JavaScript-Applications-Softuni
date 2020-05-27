ad442fc9535fa26d784ea668d9341202792fb81f
AlexKrushkova <alexkrushkova@gmail.com>
2020-05-27 09:16:27 +0300

    initial commit

---
+++ 01. Unit Testing and Modules/Exercise solutions/Tests/02.Odd-or-even-test.js
@@ -0,0 +1,34 @@
+const isOddOrEven = require('../02.Odd-Or-Even.js');
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+
+describe('isOddOrEven', function () {
+
+    // if the parameter is not  string(number)
+    //  test with number parameter -> should return undefine
+    it('should return undefined with a number parameter', function () {
+        let expected = isOddOrEven(100);
+
+        expect(expected).to.be.equal(undefined, 'Function did not return the correct result!');
+    });
+
+    // if the parameter is not string(object)
+    //  test with object parameter -> should return undefine
+    it('should return undefined with a object parameter', function () {
+        let expected = isOddOrEven({name: 'Ivo'});
+
+        expect(expected).to.be.equal(undefined, 'Function did not return the correct result!');
+    });
+
+    it('should return EVEN with string parameter with even length', function () {
+        let expected = isOddOrEven('JS4Ever!');
+
+        expect(expected).to.equal('even', 'Function return correct result with even string length')
+    });
+
+    it('should return ODD with string parameter with odd length', function () {
+        let expected = isOddOrEven('JS4Ever');
+
+        expect(expected).to.equal('odd', 'Function return correct result with odd string length')
+    });
+});
