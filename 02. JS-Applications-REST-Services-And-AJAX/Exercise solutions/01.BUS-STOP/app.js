2da8c0c85533c9ca1c7bccaca7dee2c118bc4039
AlexKrushkova <alexkrushkova@gmail.com>
2020-06-03 13:48:30 +0300

    solutions updated

--- 02. JS-Applications-REST-Services-And-AJAX/Exercise solutions/01.BUS-STOP/app.js
+++ 02. JS-Applications-REST-Services-And-AJAX/Exercise solutions/01.BUS-STOP/app.js
@@ -1,3 +1,29 @@
 function getInfo() {
-    console.log("TODO...");
+    const stopIdInput = document.getElementById('stopId');
+    const stopNameDiv = document.getElementById('stopName');
+    const bustContainer = document.getElementById('buses');
+
+
+    const buseUrl = `https://judgetests.firebaseio.com/businfo/${stopIdInput.value}.json`;
+
+    stopNameDiv.textContent = "";
+    bustContainer.innerHTML = "";
+
+    fetch(buseUrl)
+    .then(res => res.json())
+    .then(data => {
+        const { name, buses} = data;
+        stopNameDiv.textContent = name;
+        Object.entries(buses)
+        .forEach(([ busId, busTime]) => {
+            const li = document.createElement("li");
+            li.textContent = `Bus ${busId} arrives in ${busTime} minutes`;
+
+            bustContainer.appendChild(li);
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+        stopNameDiv.textContent = "Error";
+    });
 }
