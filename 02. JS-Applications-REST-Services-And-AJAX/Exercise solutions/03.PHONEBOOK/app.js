cd03823d29b51528f5135bc9201907784de6c28a
AlexKrushkova <alexkrushkova@gmail.com>
2020-06-03 14:38:26 +0300

    solution updated

--- 02. JS-Applications-REST-Services-And-AJAX/Exercise solutions/03.PHONEBOOK/app.js
+++ 02. JS-Applications-REST-Services-And-AJAX/Exercise solutions/03.PHONEBOOK/app.js
@@ -1,5 +1,65 @@
 function attachEvents() {
-    console.log('TODO...');
+
+    document.querySelector('#btnLoad')
+        .addEventListener('click', () => loadNamesAndPhones());
+
+    document.querySelector('#btnCreate')
+        .addEventListener('click', () => addNamesAndPhonesToServer());
+
+    function loadNamesAndPhones() {
+        const url = 'https://phonebook-nakov.firebaseio.com/phonebook.json';
+        fetch(url)
+            .then((request) => request.json())
+            .then((data) => {
+                document.getElementById('phonebook').innerHTML = '';
+
+                const keys = Object.keys(data);
+
+                for (const key of keys) {
+                    const name = data[key].person;
+                    const phoneNumber = data[key].phone;
+
+                    const dellBtn = document.createElement('button');
+                    dellBtn.textContent = 'Delete';
+                    dellBtn.addEventListener('click', function () {
+                        const url = `https://phonebook-nakov.firebaseio.com/phonebook/${key}.json`;
+                        fetch(url, {
+                            method: 'delete'
+                        })
+                            .then(response => response.json())
+                            .then(response => {
+                                loadNamesAndPhones();
+                            });
+                    });
+
+                    const listItem = document.createElement('li');
+                    listItem.textContent = `${name}: ${phoneNumber}`;
+                    listItem.appendChild(dellBtn);
+
+                    document.getElementById('phonebook').appendChild(listItem);
+                }
+            });
+    }
+
+    function addNamesAndPhonesToServer() {
+        const url = 'https://phonebook-nakov.firebaseio.com/phonebook.json';
+        const person = document.querySelector('#person').value;
+        const phone = document.querySelector('#phone').value;
+
+        let obj = {person, phone};
+        fetch(url, {
+            method: 'post',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify(obj)
+        })
+            .then(response => response.json())
+            .then(response => {
+                loadNamesAndPhones();
+            });
+
+        document.querySelector('#person').value = '';
+        document.querySelector('#phone').value = '';
+    }
 }
 
 attachEvents();
