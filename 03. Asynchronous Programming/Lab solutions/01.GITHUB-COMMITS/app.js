17ba20e7a3e5763df61fc3995372e73c2e428407
alexkrushkova <alexkrushkova@gmail.com>
2020-07-10 09:07:35 +0300

    Solution to 01.Github Commits updated

--- 03. Asynchronous Programming/Lab solutions/01.GITHUB-COMMITS/app.js
+++ 03. Asynchronous Programming/Lab solutions/01.GITHUB-COMMITS/app.js
@@ -1,4 +1,28 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    document.querySelector('#commits').innerHTML = '';
+
+    const userName = document.querySelector('#username').value;
+    const repository = document.querySelector('#repo').value;
+    const url = `https://api.github.com/repos/${userName}/${repository}/commits`;
+
+    fetch(url)
+        .then((response) => {
+            if (response.status >= 300) {
+                throw new Error(`${response.status} (${response.statusText})`);
+            }
+
+            return response.json();
+        })
+        .then((data) => {
+            data.forEach(commitObj => {
+                const authorName = commitObj.commit.author.name;
+                const commitMessage = commitObj.commit.message;
+
+                const liElement = `<li>${authorName}: ${commitMessage}</li>`;
+                document.querySelector('#commits').innerHTML += liElement;
+            });
+        })
+        .catch((error) => {
+            document.querySelector('#commits').innerHTML = '<li>' + error.toString() + '</li>';
+        });
 }
